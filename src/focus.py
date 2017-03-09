36523cad3e912c6ef6c9acbc86a95532823ea3db
Nicholas Omer Chiasson <nicholaschiasson@namnamnamnam.com>
2017-03-09 15:30:20 -0500

    Initial commit

---
+++ src/focus.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env pypy2 -B
+from nai.search.state import AISearchState
+#from nai.search.intelligent import alphabeta
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ src/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env pypy2 -B
+import modulepaths
+import focus
+
+def main():
+    focus.main()
+
+if __name__ == "__main__":
+    main()
